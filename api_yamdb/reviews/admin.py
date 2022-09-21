417330d42d0cc07d7ea0767f48a78aea341a0c9c
Александр Лахтюк <a.s.lakhtyuk@yandex.ru>
2022-09-21 12:19:53 +0300

    2

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
+
 from .models import Title, Category, Genre, Comment, Review
+
 admin.site.register(Title)
 admin.site.register(Category)
 admin.site.register(Genre)
