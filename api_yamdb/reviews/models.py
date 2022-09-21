3c5cf0f42d31d026920427af8c8150711e148048
Александр Лахтюк <a.s.lakhtyuk@yandex.ru>
2022-09-21 12:33:25 +0300

    2

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,7 +1,7 @@
-from django.contrib.auth import get_user_model
-
-from django.db import models
 import datetime
+
+from django.contrib.auth import get_user_model
+from django.db import models
 
 from django.core.validators import MaxValueValidator, MinValueValidator
 
